426b4e8ddcd0b1f1cf9b59dba975e6d8fcc5e01c
Chris Jackett <chris.jackett@csiro.au>
2023-05-12 12:37:44 +1000

    Adding post generation hooks for algae project.

--- templates/algae/instrument/hooks/post_gen_project.py
+++ templates/algae/instrument/hooks/post_gen_project.py
@@ -1,5 +1,18 @@
+import fnmatch
 import os
 from pathlib import Path
+
+
+def remove_gitkeep_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in fnmatch.filter(files, '.gitkeep'):
+            file_path = os.path.join(root, file)
+            try:
+                os.remove(file_path)
+                # print(f"Removed: {file_path}")
+            except OSError as e:
+                print(f"Error removing {file_path}: {e}")
+
 
 selected_instrument_id = "{{cookiecutter.instrument_id}}"
 
@@ -22,3 +35,6 @@ instrument_file.rename(lib_dir / "instrument.py")
 
 instrument_yml = instrument_path / f"{selected_instrument_id}.yml"
 instrument_yml.rename("instrument.yml")
+
+# Remove all .gitkeep files
+remove_gitkeep_files(os.getcwd())
