89ce4328362aef9c00fc60ff7a01b11fc26a9aa9
Nick Mortimer <nick.mortimer@csiro.au>
2023-09-29 08:57:59 +0800

    Updated processing with station info

--- templates/bruvs/instrument/{{cookiecutter.instrument_id}}/work/dodo.py
+++ templates/bruvs/instrument/{{cookiecutter.instrument_id}}/work/dodo.py
@@ -45,42 +45,43 @@ def task_config():
         config = {"config": get_var('config', 'NO')}
         loadconfig(config['config'])
 
-def task_import_all():
-    processes = set()
-    if platform.system() == "Linux":
-        mountpoints = pd.DataFrame(json.loads(subprocess.getoutput('lsblk -J -o  NAME,SIZE,FSTYPE,TYPE,MOUNTPOINT'))['blockdevices'])
-        mountpoints = mountpoints[~mountpoints.children.isna()]
-        mountpoints =pd.DataFrame(mountpoints.children.apply(lambda x: x[0]).to_list())[['name','mountpoint','fstype']]
-        mountpoints = mountpoints[mountpoints.fstype==format_type]           
-        paths = pd.DataFrame(subprocess.getoutput('udevadm info -q path -n $(ls /dev/s*1)').splitlines(),columns=['Path'])
-        paths[['host','dev']]=paths.Path.str.extract(r'(?P<host>host\d+).*block\/(?P<dev>([^\\]+$))')[['host','dev']]
-        paths['name'] =paths.dev.str.split('/',expand=True)[1]
-        mountpoints =mountpoints.merge(paths, on='name', how='inner')
-        commands =[]
-        for index, df in mountpoints.groupby('host'):
-            args =["import",str(COLLECTION_DIR),'BRUVS']    
-            for card in df.mountpoint.to_list():
-                args.append(card)
-            commands.append(args)
-    else:
-        commands =[]
-        for i in psutil.disk_partitions():
-            if i.fstype==format_type:
-                p =psutil.disk_usage(i.mountpoint)
-                if np.ceil(p.total/1000000000)<=512:
-                    #os.system()
-                    args =["import",str(COLLECTION_DIR),'BRUVS',i.mountpoint] 
-                    commands.append(args)
-    for args in commands:
-            yield {
-                'name':"-".join(args),
-                'actions' : [f'gnome-terminal --wait -- bash --rcfile /home/mor582/code/marimba/scripts/bashrc -ci "marimba {" ".join(args)} || :"'],
-                'uptodate':[run_once],
-                'clean':True
-            }
-@create_after(executed='import_all', target_regex='*')
+# def task_import_all():
+#     processes = set()
+#     if platform.system() == "Linux":
+#         mountpoints = pd.DataFrame(json.loads(subprocess.getoutput('lsblk -J -o  NAME,SIZE,FSTYPE,TYPE,MOUNTPOINT'))['blockdevices'])
+#         mountpoints = mountpoints[~mountpoints.children.isna()]
+#         mountpoints =pd.DataFrame(mountpoints.children.apply(lambda x: x[0]).to_list())[['name','mountpoint','fstype']]
+#         mountpoints = mountpoints[mountpoints.fstype==format_type]           
+#         paths = pd.DataFrame(subprocess.getoutput('udevadm info -q path -n $(ls /dev/s*1)').splitlines(),columns=['Path'])
+#         paths[['host','dev']]=paths.Path.str.extract(r'(?P<host>host\d+).*block\/(?P<dev>([^\\]+$))')[['host','dev']]
+#         paths['name'] =paths.dev.str.split('/',expand=True)[1]
+#         mountpoints =mountpoints.merge(paths, on='name', how='inner')
+#         commands =[]
+#         for index, df in mountpoints.groupby('host'):
+#             args =["import",str(COLLECTION_DIR),'BRUVS']    
+#             for card in df.mountpoint.to_list():
+#                 args.append(card)
+#             commands.append(args)
+#     else:
+#         commands =[]
+#         for i in psutil.disk_partitions():
+#             if i.fstype==format_type:
+#                 p =psutil.disk_usage(i.mountpoint)
+#                 if np.ceil(p.total/1000000000)<=512:
+#                     #os.system()
+#                     args =["import",str(COLLECTION_DIR),'BRUVS',i.mountpoint] 
+#                     commands.append(args)
+#     for args in commands:
+#             yield {
+#                 'name':"-".join(args),
+#                 'actions' : [f'gnome-terminal --wait -- bash --rcfile /home/mor582/code/marimba/scripts/bashrc -ci "marimba {" ".join(args)} || :"'],
+#                 'uptodate':[run_once],
+#                 'clean':True
+#             }
+# @create_after(executed='import_all', target_regex='*')
 def task_create_json():
-        for path in glob.glob(os.path.join(geturl('cardstore'),'**','100GOPRO'),recursive=True):
+        for path in glob.glob(os.path.join(geturl('cardstore'),'**','.'),recursive=True):
+            path = os.path.abspath(path)
             if glob.glob(os.path.join(path,"*.MP4")):
                 target  = os.path.join(path,cfg['paths']['exifname'])
                 file_dep = glob.glob(os.path.join(path,"*.MP4"))
@@ -93,7 +94,7 @@ def task_create_json():
                         'targets':[target],
                         'uptodate':[run_once],
                         'clean':True,
-                    } 
+                    }
 @create_after(executed='create_json', target_regex='.*\.json') 
 def task_concat_json():
 
@@ -110,15 +111,16 @@ def task_concat_json():
             data['RunTime'] = data.groupby(['CameraSerialNumber','GroupId'])['Duration'].cumsum()
             data =data.sort_values(['SourceFile'])
             data.to_csv(targets[0],index=False)
-        exiffiles =glob.glob(os.path.join(geturl('cardstore'),'**','100GOPRO',cfg['paths']['exifname']),recursive=True)
-        return { 
+        exiffiles =glob.glob(os.path.join(geturl('cardstore'),'**',cfg['paths']['exifname']),recursive=True)
+        if exiffiles:
+            return { 
 
-            'file_dep':exiffiles,
-            'actions':[concat],
-            'targets':[geturl('exifstore')],
-            'uptodate':[True],
-            'clean':True,
-        } 
+                'file_dep':exiffiles,
+                'actions':[concat],
+                'targets':[geturl('exifstore')],
+                'uptodate':[True],
+                'clean':True,
+            } 
 
 @create_after(executed='concat_json', target_regex='.*\.json') 
 def task_make_autodeployments():
@@ -185,34 +187,52 @@ def matchbars(deployments,barnumbers,datecolumn='CreateDate'):
     result['DeploymentId']=result.apply(lambda x: f"{x.CreateDate.strftime('%Y%m%dT%H%M%S')}_{x.Frame}_{x.GoProNumber}_{x.CameraSerialNumber}", axis=1)
     return result
 
+
 @create_after(executed='make_autodeployments', target_regex='.*\.json') 
 def task_make_matchbars():
 
         def stagedeployments(dependencies, targets):
             def calculatetimes(df):
+                df =df.sort_values('ItemId')
                 if len(df)>1:
-                    start = df.RunTime.shift(+1)
-                    start.iloc[0] = pd.to_timedelta(0,unit='S')
-                    start = pd.to_timedelta(start,unit='S')
-                    return(df['CreateDate']+start).dt.round('1S')                 
-                return(df['CorrectedTime'])
-            
+                    start = df.Duration.cumsum().shift(+1)
+                    start.iloc[0] = 0
+                    start =pd.to_timedelta(start,unit='S')
+                    df['CalculatedStartTime']=(df['CorrectedTime']+start).dt.round('1S')
+                else:                 
+                    df['CalculatedStartTime']=df['CorrectedTime']
+                return df
+            def makedeploymentkey(df):
+                starttime =df.CorrectedTime.min().strftime("%Y%m%dT%H%M%S")
+                totals =df.groupby('GoProNumber').count().reset_index()
+                left = 0 if len(totals[totals.GoProNumber.str.startswith('L')])==0 else totals[totals.GoProNumber.str.startswith('L')].iloc[0].CreateDate
+                right =0 if len(totals[totals.GoProNumber.str.startswith('R')])==0 else totals[totals.GoProNumber.str.startswith('R')].iloc[0].CreateDate
+                df['StageId']=df.Frame+'_'+starttime
+                if left==right:
+                    df['StageDir']=df.Frame+'_'+starttime+f'_{left:02}'
+                else:
+                    df['StageDir']=df.Frame+'_'+starttime+f'_{left:02}_{right:02}'
+                return df
             dep = pd.read_csv(geturl('autodeployment'),parse_dates=['CreateDate'])
             exifdata = pd.read_csv(geturl('exifstore'),parse_dates=['CreateDate']).set_index(['CreateDate','CameraSerialNumber','GroupId'])
             correcttimes = pd.read_csv(geturl('timecorrection'),parse_dates=['CreateDate','CorrectedTime'])
             dep =pd.merge(dep,correcttimes[['DeploymentId','CorrectedTime']],on='DeploymentId', how='left').set_index(['CreateDate','CameraSerialNumber','GroupId'])
             combined = dep.join(exifdata,rsuffix='_exif').reset_index()
-            combined['CalculatedStartTime'] =combined.groupby(['CreateDate','CameraSerialNumber','GroupId']).apply(calculatetimes).reset_index()[0]
+            combined =combined.drop_duplicates(subset=['CameraSerialNumber','CreateDate','GroupId','ItemId'],keep='last')
+            combined = combined.sort_values(['CorrectedTime','GroupId','ItemId'])
+            combined =combined.groupby(['CreateDate','CameraSerialNumber','GroupId']).apply(calculatetimes).reset_index()
             barpath = f'{CATALOG_DIR}/camerabars.csv'
             barnumbers = pd.read_csv(barpath,parse_dates=['BarStartDate','BarEndDate']) 
             result = matchbars(combined,barnumbers,datecolumn='CalculatedStartTime')
             result['CalculatedStartTime'] = pd.to_datetime(result['CalculatedStartTime'])
-            result['StageName'] = result.apply(lambda x: f'{x.Frame}_{x.GoProNumber}_{x.CalculatedStartTime.strftime("%Y%m%dT%H%M%S")}_{x.GroupId:02d}_{x.ItemId:02d}_{x.CameraSerialNumber}.MP4',axis=1)
-            result['StageDir'] = result.apply(lambda x: f'{x.CreateDate.strftime("%Y%m%dT%H%M%S")}_{x.Frame}_{x.GoProNumber}_{x.CameraSerialNumber}',axis=1)
+            result['CorrectedTime'] = pd.to_datetime(result['CorrectedTime'])
+            result['StageName'] = result.apply(lambda x: f'{x.Frame}_{x.GoProNumber}_{x.CalculatedStartTime.strftime("%Y%m%dT%H%M%S")}_{x.CameraSerialNumber}_{int(x.GroupId):02d}_{int(x.ItemId):02d}.MP4',axis=1)
+            result =result.drop_duplicates(subset=['CameraSerialNumber','CreateDate','GroupId','ItemId'],keep='last')
+            result=result.groupby(['Frame',pd.Grouper(key='CorrectedTime',freq='5min')],group_keys=True).apply(makedeploymentkey)
             result.to_csv(targets[0],index=False)
         return { 
 
-            'file_dep':[geturl('autodeployment'),geturl('timecorrection'),geturl('exifstore')],
+            'file_dep':[geturl('autodeployment'),geturl('timecorrection'),geturl('exifstore'),f'{CATALOG_DIR}/camerabars.csv'],
             'actions':[stagedeployments],
             'targets':[geturl('stage')],
             'uptodate':[True],
@@ -243,17 +263,45 @@ def task_stage_data():
 
                
         if os.path.exists(geturl('stage')):
-             stage = pd.read_csv(geturl('stage'))
-             targets =stage.apply(lambda x: os.path.join(CATALOG_DIR,'stage',x.StageDir,x.StageName),axis=1).to_list()
+            stage = pd.read_csv(geturl('stage'))
+            targets =stage.apply(lambda x: os.path.join(CATALOG_DIR,'stage',x.StageDir,x.StageName),axis=1).unique().tolist()
+            return { 
+
+                'file_dep':[geturl('stage')],
+                'actions':[hardlink],
+                'targets':targets,
+                'uptodate':[True],
+                'clean':[clean_targets,delete_empty_folders],
+            } 
+        
+@create_after(executed='make_matchbars', target_regex='.*\.json') 
+def task_update_stationinformation():
+        def finalnames(dependencies, targets):
+             stage = pd.read_csv(geturl('stage'),index_col='StageId')
+             stationinfo = targets[0]
+             stations = pd.read_csv(stationinfo,index_col='StageId')
+             stations =stations.join(stage.groupby('StageId').first(),how='outer')[stations.columns]
+             stations.to_csv(targets[0])          
+        targets =geturl('commit')
         return { 
 
             'file_dep':[geturl('stage')],
-            'actions':[hardlink],
-            'targets':targets,
+            'actions':[finalnames],
+            'targets':[geturl('stationinfo')],
             'uptodate':[True],
-            'clean':[clean_targets,delete_empty_folders],
+            'clean':True,
         } 
+        
 
+def delete_empty_folders(dryrun):
+    for dirpath, dirnames, filenames in os.walk(os.path.join(CATALOG_DIR,'stage'), topdown=False):
+        for dirname in dirnames:
+            full_path = os.path.join(dirpath, dirname)
+            if not os.listdir(full_path): 
+                if dryrun:
+                        print(f'Remove dir {full_path}')
+                else:
+                    os.rmdir(full_path)
 
 if __name__ == '__main__':
     import doit
