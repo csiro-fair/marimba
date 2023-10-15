ddf6cdc05c7fd3cf10163543c7744fe6d87189e1
Nick Mortimer <nick.mortimer@csiro.au>
2023-10-15 21:26:32 +0800

    initialise spelling correction

--- tests/initalise.py
+++
@@ -1,5 +0,0 @@
-import sys
-from marimba.marimba import marimba
-
-if __name__ == '__main__':
-    marimba(["initialise","/media/mor582/Video1/data/DR2023-02","BRUVS", "/media/mor582/Video1/DR2023-02/fielddata/BRUVS" ])
