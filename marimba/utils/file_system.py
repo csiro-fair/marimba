3206fc07c695c5d358cc967c5b6bd82e50ba7c1e
Kevin Barnard <kbarnard@mbari.org>
2023-10-23 11:36:46 +1100

    Update file_system util module for pathlib

--- marimba/utils/file_system.py
+++ marimba/utils/file_system.py
@@ -3,22 +3,25 @@ File system utils
 """
 
 import os
+from pathlib import Path
+from typing import Union
 
 from marimba.utils.log import get_collection_logger
 
 logger = get_collection_logger()
 
 
-def create_directory_if_necessary(path: str):
+def create_directory_if_necessary(path: Union[str, Path]):
     """
     Create a directory if it doesn't already exist.
 
     Args:
         path: The path to the directory.
     """
-    if not os.path.isdir(path):
+    path = Path(path)  # convert to Path object if necessary
+    if not path.is_dir():
         try:
             logger.info(f"Creating new directory path: {path}")
-            os.makedirs(path)
-        except OSError as error:
+            path.mkdir(parents=True)
+        except Exception as error:
             logger.error(error)
