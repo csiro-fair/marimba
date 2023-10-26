3764e125dfedf6af4bf0188d862c2bd4ed901a62
Kevin Barnard <kbarnard@mbari.org>
2023-10-26 16:24:13 +1100

    Restructure base_instrument

--- marimba/core/base_instrument.py
+++ marimba/core/base_instrument.py
@@ -1,5 +1,8 @@
-from abc import ABC
-from typing import Optional
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
+
+from ifdo import iFDO
 
 from marimba.utils.log import LogMixin
 
@@ -73,26 +76,150 @@ class BaseInstrument(ABC, LogMixin):
         """
         return self.__class__.__name__
 
-    def run_init_or_import(self, command_name, kwargs):
-        command = getattr(self, command_name)
-        command(**kwargs)
+    def run_init(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        Public interface for the init command. Delegate to the private implementation method `_init`.
 
-    def run_catalog(self, data_dir, config, **kwargs):
-        self.logger.warning(f"There is no MarImBA [bold]catalog[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+        Do not override this method. Override `_init` instead.
 
-    def run_metadata(self, data_dir, config, **kwargs):
-        self.logger.warning(f"There is no MarImBA [bold]metadata[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+        Args:
+            data_dir: The data directory.
+            config: The deployment configuration.
+            kwargs: Additional keyword arguments.
+        """
+        self.logger.debug(
+            f"Running [bold]init[/bold] command for instrument [bold]{self.class_name}[/bold] with args: {data_dir=}, {config=}, {kwargs=}"
+        )
+        return self._init(self, data_dir, config, **kwargs)
 
-    def run_package(self, data_dir, config, **kwargs):
-        self.logger.warning(f"There is no MarImBA [bold]package[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+    def run_import(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        Public interface for the import command. Delegate to the private implementation method `_import`.
 
-    def run_process(self, data_dir, config, **kwargs):
-        self.logger.warning(f"There is no MarImBA [bold]process[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+        Do not override this method. Override `_import` instead.
 
-    def run_rename(self, data_dir, config, **kwargs):
+        Args:
+            data_dir: The data directory.
+            config: The deployment configuration.
+            kwargs: Additional keyword arguments.
+        """
+        self.logger.debug(
+            f"Running [bold]import[/bold] command for instrument [bold]{self.class_name}[/bold] with args: {data_dir=}, {config=}, {kwargs=}"
+        )
+        return self._import(data_dir, config, **kwargs)
+
+    def run_rename(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        Public interface for the rename command. Delegate to the private implementation method `_rename`.
+
+        Do not override this method. Override `_rename` instead.
+
+        Args:
+            data_dir: The data directory.
+            config: The deployment configuration.
+            kwargs: Additional keyword arguments.
+        """
+        self.logger.debug(
+            f"Running [bold]rename[/bold] command for instrument [bold]{self.class_name}[/bold] with args: {data_dir=}, {config=}, {kwargs=}"
+        )
+        return self._rename(data_dir, config, **kwargs)
+
+    def run_process(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        Public interface for the process command. Delegate to the private implementation method `_process`.
+
+        Do not override this method. Override `_process` instead.
+
+        Args:
+            data_dir: The data directory.
+            config: The deployment configuration.
+            kwargs: Additional keyword arguments.
+        """
+        self.logger.debug(
+            f"Running [bold]process[/bold] command for instrument [bold]{self.class_name}[/bold] with args: {data_dir=}, {config=}, {kwargs=}"
+        )
+        return self._process(data_dir, config, **kwargs)
+
+    def run_metadata(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        Public interface for the metadata command. Delegate to the private implementation method `_metadata`.
+
+        Do not override this method. Override `_metadata` instead.
+
+        Args:
+            data_dir: The data directory.
+            config: The deployment configuration.
+            kwargs: Additional keyword arguments.
+        """
+        self.logger.debug(
+            f"Running [bold]metadata[/bold] command for instrument [bold]{self.class_name}[/bold] with args: {data_dir=}, {config=}, {kwargs=}"
+        )
+        return self._metadata(data_dir, config, **kwargs)
+
+    def run_compose(self, data_dirs: List[Path], configs: List[Dict[str, Any]], **kwargs: dict) -> Tuple[iFDO, Dict[Path, Path]]:
+        """
+        Compose a dataset from the given data directories and their corresponding deployment configurations.
+
+        Return an [iFDO](https://marine-imaging.com/fair/ifdos/iFDO-overview/) instance that represents the composed dataset and a dictionary that maps files within the provided data directories to relative paths for the resulting distributable dataset.
+
+        Args:
+            data_dirs: The data directories to compose.
+            configs: The deployment configurations for the data directories.
+            kwargs: Additional keyword arguments.
+
+        Returns:
+            The iFDO and path mapping dict.
+        """
+        self.logger.debug(
+            f"Running [bold]compose[/bold] command for instrument [bold]{self.class_name}[/bold] with args: {data_dirs=}, {configs=}, {kwargs=}"
+        )
+        return self._compose(data_dirs, configs, **kwargs)
+
+    def _init(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        `run_init` implementation; override this to implement the init command.
+
+        TODO: Add docs on how to implement this method.
+        """
+        self.logger.warning(f"There is no MarImBA [bold]init[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+
+    def _import(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        `run_import` implementation; override this to implement the import command.
+
+        TODO: Add docs on how to implement this method.
+        """
+        self.logger.warning(f"There is no MarImBA [bold]import[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+
+    def _rename(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        `run_rename` implementation; override this.
+
+        TODO: Add docs on how to implement this method.
+        """
         self.logger.warning(f"There is no MarImBA [bold]rename[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
 
-    # def run_report(self):
-    #     self.logger.warning(
-    #         f'There is no MarImBA [bold]report[/bold] command implemented for instrument [bold]{self.instrument_config.get("id")}[/bold]'
-    #     )
+    def _process(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        `run_process` implementation; override this to implement the process command.
+
+        TODO: Add docs on how to implement this method.
+        """
+        self.logger.warning(f"There is no MarImBA [bold]process[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+
+    def _metadata(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+        """
+        `run_metadata` implementation; override this to implement the metadata command.
+
+        TODO: Add docs on how to implement this method.
+        """
+        self.logger.warning(f"There is no MarImBA [bold]init[/bold] command implemented for instrument [bold]{self.class_name}[/bold]")
+
+    @abstractmethod
+    def _compose(self, data_dirs: List[Path], configs: List[Dict[str, Any]], **kwargs: dict) -> Tuple[iFDO, Dict[Path, Path]]:
+        """
+        `run_compose` implementation; override this.
+
+        TODO: Add docs on how to implement this method.
+        """
+        raise NotImplementedError
